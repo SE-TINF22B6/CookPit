44fc164e9e4cad0c530f9f9652796d6ed582ad9b
Dominik <115451772+INeeedADoc@users.noreply.github.com>
2023-12-06 20:00:20 +0100

    Fix--PopUp von Karim gefixt.

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -5,15 +5,20 @@ import Background from '../Background/Background';
 import Login from '../Login/Login';
 import SearchSite from '../SearchSite/SearchSite';
 import OpenAI from '../openai/OpenAI';
+import React, { useState } from 'react';
 
 function Home() {
+  const [loginVisible, setLoginVisible] = useState(false);
 
+  const toggleLoginVisibility = () => {
+    setLoginVisible(!loginVisible);
+  };
 
   return (
     <>
       <Background />
-      <Header />
-      <Login />
+      <Header onToggleLogin={toggleLoginVisibility} />
+      {loginVisible && <Login />}
       <Routes>
           <Route path='/' element={<SearchSite />} />
           <Route path='/recipe-maker' element={<OpenAI />} />
