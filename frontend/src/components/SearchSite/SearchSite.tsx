dadb7b4cb76fd0e3e16743961cd3bb836ec6af27
Karim Gharbi <gharbi.karim@outlook.de>
2024-04-16 13:52:07 +0200

    added styling to fav list

--- frontend/src/components/SearchSite/SearchSite.tsx
+++ frontend/src/components/SearchSite/SearchSite.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import '../SearchSite/SearchSite.css';
 import icon_search from '../SearchSite/baseline_search_black_24dp.png'
-
-
+import left from '../../img/Pfannkuchen.png';
+import middle from '../../img/Donut.png';
+import right from '../../img/Schokokuchen.png';
+import { BsClock } from "react-icons/bs";
+import { AiOutlineStar } from "react-icons/ai";
+import { VscChevronLeft } from "react-icons/vsc";
+import { VscChevronRight } from "react-icons/vsc";
 
 export default function Body(){
     function setLanguage(language:HTMLElement) {
@@ -28,25 +33,50 @@ export default function Body(){
 <br />
 <br />
 <br />
+
 <div id='mainArea'>
-<div id='arrowLeft'>left</div>
+    
+    {/*<div id='arrowLeft'><VscChevronLeft /></div>*/}
 
-<div id='products'>
+        <div id='products'>
 
-<div id='left' > 
-    left product
-    <img src="" alt="" />
+            <div id='left' > 
+                <img id='picture_Left'  src={left} alt="favourite recipes" />
+                <div id='footerleft'>
+                    <h4>           
+                        Pfannkuchen <br></br>
+                        <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                    </h4>
+                </div>
+            </div>
+
+
+            <div id='middle' > 
+                <img id='picture_Middle'  src={middle} alt="favourite recipes" />
+                <div id='footermiddle'>
+                    <h4>           
+                        Donuts <br></br>
+                        <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                    </h4>
+                </div>
+            </div>
+
+
+            <div id='right' > 
+                <img id='picture_Right'  src={right} alt="favourite recipes" />
+                <div id='footerright'>
+                    <h4>           
+                        Schokokuchen <br></br>
+                        <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                    </h4>
+                </div> 
+            </div> 
+
+        </div>
+        
+        {/*<div id='arrowRight'><VscChevronRight /> </div>*/}
 </div>
-
-
-<div id='middle' > middle ptoduct</div>
-
-
-<div id='right' > right product </div> 
-
-</div>
-
-<div id='arrowRight'>right </div>
-</div>
+<button id='arrleft'> <div></div> </button>
+<button id='arrright'> <div></div> </button>
 </body>
   )}
