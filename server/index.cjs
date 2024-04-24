daed565f7dca46afc058c2e2fab475df96891e4c
Simon <sihajek@aol.com>
2024-04-24 17:17:06 +0200

    new api key; add .env into .gitignore;

--- server/index.cjs
+++ server/index.cjs
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.post("/recipe-maker", async (req, res) => {
-console.log("Empfangene Daten:", req.body);
+  console.log("Empfangene Daten:", req.body);
   try {
     const importedModule = await import("./openai-test.mjs");
     const { callOpenAIapi } = importedModule;
@@ -27,37 +28,44 @@ app.listen(PORT, () => {
 });
 
 // Datenbank erstellung, initalisierung usw.
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./account.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(
+  "./account.db",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
     if (err) {
-        console.error("Fehler beim Öffnen der Datenbank: " + err.message);
+      console.error("Fehler beim Öffnen der Datenbank: " + err.message);
     } else {
-        console.log("Datenbank erfolgreich geöffnet.");
-        createTables();
+      console.log("Datenbank erfolgreich geöffnet.");
+      createTables();
     }
-});
+  }
+);
 
 function createTables() {
-    db.run('CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)', (err) => {
-        if (err) {
-            console.error("Fehler beim Erstellen der Tabelle: " + err.message);
-        } else {
-            console.log("Tabelle 'account' erfolgreich erstellt.");
-        }
-    });
+  db.run(
+    "CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)",
+    (err) => {
+      if (err) {
+        console.error("Fehler beim Erstellen der Tabelle: " + err.message);
+      } else {
+        console.log("Tabelle 'account' erfolgreich erstellt.");
+      }
+    }
+  );
 }
 
 function insertData() {
-      let sql = 'INSERT INTO account (username, password) VALUES (?, ?)';
-      let params = ["Fred", "01231245"];
-      db.run(sql, params, function(err) {
-          if (err) {
-              console.error("Fehler beim Einfügen von Daten: " + err.message);
-          } else {
-              console.log(`Datensatz hinzugefügt mit der ID ${this.lastID}.`);
-          }
-      });
-  }
+  let sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+  let params = ["Fred", "01231245"];
+  db.run(sql, params, function (err) {
+    if (err) {
+      console.error("Fehler beim Einfügen von Daten: " + err.message);
+    } else {
+      console.log(`Datensatz hinzugefügt mit der ID ${this.lastID}.`);
+    }
+  });
+}
 
 /*function checkUser(username) {
   db.get("SELECT * FROM account WHERE username = ?",
@@ -81,7 +89,8 @@ app.post("/register", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
 
-  db.run("INSERT INTO account (username, password) VALUES (?, ?)",
+  db.run(
+    "INSERT INTO account (username, password) VALUES (?, ?)",
     [username, password],
     (err, result) => {
       if (err) {
@@ -91,27 +100,31 @@ app.post("/register", (req, res) => {
         console.log("Daten erfolgreich eingefügt.");
         res.status(200).send("Erfolgreich registriert.");
       }
-    });
+    }
+  );
 });
 
 // Login
 app.post("/login", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
-  
-  db.get("SELECT * FROM account WHERE username = ? AND password = ?",
+
+  db.get(
+    "SELECT * FROM account WHERE username = ? AND password = ?",
     [username, password],
     (err, result) => {
       if (err) {
-        res.send({loginmessage: err});
+        res.send({ loginmessage: err });
         return;
       }
       if (result) {
-        res.send({loginmessage: "Erfolgreich eingeloggt"});
-        console.log('User "'+ username+'" eingeloggt');
+        res.send({ loginmessage: "Erfolgreich eingeloggt" });
+        console.log('User "' + username + '" eingeloggt');
       } else {
-        res.send({ loginmessage: "Der Nutzername stimmt nicht mit dem Passwort überein"});
-        console.log('User "'+ username+'" existiert nicht');
+        res.send({
+          loginmessage: "Der Nutzername stimmt nicht mit dem Passwort überein",
+        });
+        console.log('User "' + username + '" existiert nicht');
       }
     }
   );
