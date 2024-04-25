ea58b223d0cdbbb5e09b362ec2d442aee76e7889
Simon Hajek <sihajek@aol.com>
2024-04-25 10:49:59 +0200

    fixed menu

--- frontend/src/components/SearchSite/SearchSite.tsx
+++ frontend/src/components/SearchSite/SearchSite.tsx
@@ -15,70 +15,66 @@ export default function Body(){
         activeLanguage!.innerText = language.innerText;}
     
   return (
-<div id='wrapper'>
-    <div id='searchBoxContainer'>
-        <table id='searchElementsContainer'>
-            <tr>
-                <td>
-                    <input type="text" placeholder='Search' id='searchInput'  />
-                </td>
-                <td>
-                    <a id='searchIcon' href="#">
-                    <img src={icon_search} alt="#"/>
-                    </a>
-                </td>
-            </tr>
-        </table>
-    </div>
-    <br />
-    <br />
-    <br />
+    <div id='wrapper'>
+        <div id='searchBoxContainer'>
+            <table id='searchElementsContainer'>
+                <tr>
+                    <td>
+                        <input type="text" placeholder='Search' id='searchInput'  />
+                    </td>
+                    <td>
+                        <a id='searchIcon' href="#">
+                        <img src={icon_search} alt="#"/>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
 
-
-    <div id='mainArea'>
-        
-        {/*<div id='arrowLeft'><VscChevronLeft /></div>*/}
-
-            <div id='products'>
-
-                <div id='left' > 
-                    <img id='picture_Left'  src={left} alt="favourite recipes" />
-                    <div id='footerleft'>
-                        <h4>           
-                            Pfannkuchen <br></br>
-                            <BsClock /> 5 min        <AiOutlineStar /> 4/5
-                        </h4>
-                    </div>
-                </div>
-
-
-                <div id='middle' > 
-                    <img id='picture_Middle'  src={middle} alt="favourite recipes" />
-                    <div id='footermiddle'>
-                        <h4>           
-                            Donuts <br></br>
-                            <BsClock /> 5 min        <AiOutlineStar /> 4/5
-                        </h4>
-                    </div>
-                </div>
-
-
-                <div id='right' > 
-                    <img id='picture_Right'  src={right} alt="favourite recipes" />
-                    <div id='footerright'>
-                        <h4>           
-                            Schokokuchen <br></br>
-                            <BsClock /> 5 min        <AiOutlineStar /> 4/5
-                        </h4>
-                    </div> 
-                </div> 
-
-            </div>
+        <div id='mainArea'>
             
-            {/*<div id='arrowRight'><VscChevronRight /> </div>*/}
+            {/* <div id='arrowLeft'><VscChevronLeft /></div> */}
 
+                <div id='products'>
+
+                    <div id='left' > 
+                        <img id='picture_Left'  src={left} alt="favourite recipes" />
+                        <div id='footerleft'>
+                            <h4>           
+                                Pfannkuchen <br></br>
+                                <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                            </h4>
+                        </div>
+                    </div>
+
+
+                    <div id='middle' > 
+                        <img id='picture_Middle'  src={middle} alt="favourite recipes" />
+                        <div id='footermiddle'>
+                            <h4>           
+                                Donuts <br></br>
+                                <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                            </h4>
+                        </div>
+                    </div>
+
+
+                    <div id='right' > 
+                        <img id='picture_Right'  src={right} alt="favourite recipes" />
+                        <div id='footerright'>
+                            <h4>           
+                                Schokokuchen <br></br>
+                                <BsClock /> 5 min        <AiOutlineStar /> 4/5
+                            </h4>
+                        </div> 
+                    </div> 
+
+                </div>
+                
+                {/* <div id='arrowRight'><VscChevronRight /> </div> */}
+
+        </div>
+        <button id='arrleft'> <div></div> </button>
+        <button id='arrright'> <div></div> </button>
     </div>
-    <button id='arrleft'> <div></div> </button>
-    <button id='arrright'> <div></div> </button>
-</div>
   )}
