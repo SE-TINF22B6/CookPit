3afd3e73a52cf16054dc4c1d54dfe8399989751d
Simon Hajek <sihajek@aol.com>
2024-04-25 09:56:28 +0200

    fix 1

--- frontend/src/components/Background/Background.tsx
+++ frontend/src/components/Background/Background.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../Background/Background.css';
 
 
@@ -8,22 +8,24 @@ export default function Background(){
         const activeLanguage = document.getElementById("language_selection");
         activeLanguage!.innerText = language.innerText;
       }
+
+     useEffect(() => {
+      let numOfPillar = Math.floor(window.innerWidth / 130);
+      const ovalWrapper = document.getElementById("oval_wrapper");
+      
+      // Clear existing divs
+      while (ovalWrapper?.firstChild) {
+        ovalWrapper.removeChild(ovalWrapper.firstChild);
+      }
+  
+      for (let index = 0; index < numOfPillar; index++) {
+        const div = document.createElement("div");
+        div.className = "oval";
+        ovalWrapper?.appendChild(div);
+      }
+    }, []);
+
+
   return (
-<body>
-  <table className='background'> <tr>
-   <td><div id="oval1"></div> </td> 
-   <td><div id="oval2"></div></td> 
-   <td> <div id="oval3"></div></td> 
-   <td><div id="oval4"></div> </td> 
-   <td><div id="oval5"></div></td> 
-   <td><div id="oval6"></div></td> 
-   <td><div id="oval7"></div></td> 
-   <td><div id="oval8"></div></td> 
-   <td><div id="oval9"></div> </td> 
-   <td><div id="oval10"></div></td> 
-   <td><div id="oval11"></div></td> 
-   <td><div id="oval12"></div></td> 
-   <td><div id="oval13"></div></td> 
-    </tr></table>
-</body>
+    <div id="oval_wrapper"></div>
   )}
--- frontend/src/components/SearchSite/SearchSite.tsx
+++ frontend/src/components/SearchSite/SearchSite.tsx
@@ -10,28 +10,25 @@ export default function Body(){
         activeLanguage!.innerText = language.innerText;}
     
   return (
-<body>
-<div id='searchBoxContainer'>
-    <table id='searchElementsContainer'>
-        <tr>
-            <td>
-                <input type="text" placeholder='Search' id='searchInput'  />
-            </td>
-            <td>
-                <a id='searchIcon' href="#">
-                <img src={icon_search} alt="#"/>
-                </a>
-            </td>
-        </tr>
-    </table>
-</div>
-<br />
-<br />
-<br />
-<div id='mainArea'>
-<div id='arrowLeft'></div>
-<div id='placeholder' > here can be your recipe</div> 
-<div id='arrowRight'>Hi</div>
-</div>
-</body>
+    <div id='wrapper'>
+        <div id='searchBoxContainer'>
+            <table id='searchElementsContainer'>
+                <tr>
+                    <td>
+                        <input type="text" placeholder='Search' id='searchInput'  />
+                    </td>
+                    <td>
+                        <a id='searchIcon' href="#">
+                        <img src={icon_search} alt="#"/>
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div id='mainArea'>
+            <div id='arrowLeft'></div>
+            <div id='placeholder' > here can be your recipe</div> 
+            <div id='arrowRight'>Hi</div>
+        </div>
+    </div>
   )}
