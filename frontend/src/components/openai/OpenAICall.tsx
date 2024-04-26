95cf212ad2aaeb6ee597e637199f426d2de26444
Simon <sihajek@aol.com>
2024-04-26 20:07:41 +0200

    add small description

--- frontend/src/components/openai/OpenAICall.tsx
+++ frontend/src/components/openai/OpenAICall.tsx
@@ -153,6 +153,16 @@ export default function OpenAICall() {
               <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
             </svg>
           </button>
+          <div id="how_to_use" className="hover-text">
+            ?
+            <span id="tooltip">
+              Gebe zunächst all deine Zutaten einzeln in die Eingabe ein.
+              Anschließend klickst du auf "Rezept erstellen". Daraufhin wird
+              basierend auf deiner erstellten Zutatenliste ein Rezept für dich
+              erstellt. Über das Downloadsymbol kannst du dir das Rezept als
+              PDF-Datei abspeichern.
+            </span>
+          </div>
         </div>
       </div>
     </>
