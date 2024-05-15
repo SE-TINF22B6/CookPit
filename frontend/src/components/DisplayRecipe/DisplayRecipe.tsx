fe7dd019b08e8a4f7d0452da7b947de12c208de0
Simon Hajek <sihajek@aol.com>
2024-05-15 10:19:06 +0200

    added title

--- frontend/src/components/DisplayRecipe/DisplayRecipe.tsx
+++ frontend/src/components/DisplayRecipe/DisplayRecipe.tsx
@@ -69,7 +69,7 @@ export default function DisplayRecipe({
       >
         <Box sx={style} onClick={(event) => event.stopPropagation()}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            {title}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
