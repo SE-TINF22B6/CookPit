9e279d9c19fe39401e000bc346ca9ab2bd41c04a
Simon <sihajek@aol.com>
2024-05-15 21:32:45 +0200

    minor fixes

--- frontend/src/components/AllRecipes/AllRecipes.tsx
+++ frontend/src/components/AllRecipes/AllRecipes.tsx
@@ -1,23 +1,22 @@
-import DisplayRecipe from '../DisplayRecipe/DisplayRecipe'
-import './AllRecipes.css'
+import DisplayRecipe from "../DisplayRecipe/DisplayRecipe";
+import "./AllRecipes.css";
 
 export default function AllRecipes({ allRecipes }: { allRecipes: any[] }) {
-
-    const items = allRecipes.map((recipe) => {  
-        const time = Number(recipe.cook_time) + Number(recipe.prep_time);
-        return (
-          <DisplayRecipe
-            img={recipe.img}
-            title={recipe.name}
-            rating={recipe.rating}
-            time={time}
-          />
-        );
-      });
+  const items = allRecipes.map((recipe) => {
+    const time = Number(recipe.cook_time) + Number(recipe.prep_time);
+    return (
+      <DisplayRecipe
+        img={recipe.img}
+        title={recipe.name}
+        rating={recipe.rating}
+        time={time}
+      />
+    );
+  });
 
   return (
-    <div id='wrapper'>
-        {items}
+    <div id="wrapper">
+      <div id="center_wrapper">{items}</div>
     </div>
-  )
+  );
 }
