aada3296cde8bdb83f2ba509c0e90640d6db77eb
Simon Hajek <sihajek@aol.com>
2024-05-16 10:17:29 +0200

    change modal size + responsive

--- frontend/src/components/DisplayRecipe/DisplayRecipe.tsx
+++ frontend/src/components/DisplayRecipe/DisplayRecipe.tsx
@@ -35,7 +35,7 @@ export default function DisplayRecipe({
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    maxWidth: 800,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
