8361a17623f36b37b6fcacf3ead1b77a2591acc0
Karim Gharbi <gharbi.karim@outlook.de>
2024-05-21 16:28:55 +0200

    edited design and fixed scroll problem

--- frontend/src/components/recipeUpload/recipeUpload.tsx
+++ frontend/src/components/recipeUpload/recipeUpload.tsx
@@ -102,99 +102,100 @@ export default function Body(){
     
       
 return (
-<div id='upload_wrapper'>
- 
-    <div id='blockerTop'></div>
-    <div id='template'>
+<body>
+  <div id='template'>
     <div className="form-container">
  
-    <div className='recipeInfo'>
-     <div className="form-group">
-          <label htmlFor="headingTop">Header</label>
-          <input type="text" id="headingTop" name="Heading" onChange={(e) => { setheader(e.target.value); }} />
+      <div className='recipeInfo'>
+      <div className="form-group">
+            <label htmlFor="headingTop">Header</label>
+            <input type="text" id="headingTop" name="Heading" onChange={(e) => { setheader(e.target.value); }} />
         </div>
-        
+          
         <div className="form-groupDate">
-          <label htmlFor="headingTop">Date: {currentDate.toLocaleString()}</label>
+            <label htmlFor="headingTop">Date: {currentDate.toLocaleString()}</label>
         </div>
-       
-       
-       </div>
+        
+        
+      </div>
+      <div className='wrapperInput'>
         <div className='recipeInfo'>
-        <div className="form-groupCategory">
-          <label htmlFor="headingTop">Category</label>
-          <input type="text" id="headingTop" name="Category" onChange={(e) => { setcategory(e.target.value); }} />
+          <div className="form-groupCategory">
+            <label htmlFor="headingTop">Category</label>
+            <input type="text" id="headingTop" name="Category" onChange={(e) => { setcategory(e.target.value); }} />
+          </div>
+
+        
+
+          <div className="form-groupTimeEffort">
+            <label htmlFor="headingTop">Time effort</label>
+            <input type="text" id="headingTop" name="Effort" onChange={(e) => { settimeeffort(e.target.value); }} />
+          </div>
+          
+          {/*<div className="form-groupCalories">
+            <label htmlFor="headingTop">Calories</label>
+            <input type="text" id="headingTop" name="Calories" onChange={(e) => { setcalories(e.target.value); }} />
+          </div>*/}
+          
+          <div className="rating">
+            {[...Array(5)].map((_, index) => (
+              <React.Fragment key={index}>
+                <input value={5- index} name="rate" id={`star${5- index}`} type="radio" onChange={handleRatingChange} checked={stars === `${5- index}`} />
+                <label title={`${5- index} Sterne`} htmlFor={`star${5- index}`}></label>
+              </React.Fragment>
+            ))}
+          </div>
+          
         </div>
 
+
+          
+        {/* <div id="heading" >
+                <input type="text"/>
+                <span>Heading</span>
+            </div>*/}
+        <div id='blocker1'></div>
+        <div className='uploadpicture'>
+          <img className='imageUpload' src={uploadImageSrc || uploadImage} id='picture-pic' alt="Uploaded" />
+        <label className='uploadImageLabel' htmlFor="input-file">Upload Image</label>
+        <input id='input-file' type="file" accept='image/jpeg, image/png, image/jpg' onChange={handleFileChange} />
+        </div>
+      </div>
       
-
-        <div className="form-groupTimeEffort">
-          <label htmlFor="headingTop">Time effort</label>
-          <input type="text" id="headingTop" name="Effort" onChange={(e) => { settimeeffort(e.target.value); }} />
-        </div>
-
-        <div className="form-groupCalories">
-          <label htmlFor="headingTop">Calories</label>
-          <input type="text" id="headingTop" name="Calories" onChange={(e) => { setcalories(e.target.value); }} />
-        </div>
-
-        <div className="rating">
-          {[...Array(5)].map((_, index) => (
-            <React.Fragment key={index}>
-              <input value={5- index} name="rate" id={`star${5- index}`} type="radio" onChange={handleRatingChange} checked={stars === `${5- index}`} />
-              <label title={`${5- index} Sterne`} htmlFor={`star${5- index}`}></label>
-            </React.Fragment>
-          ))}
-        </div>
-        
-        </div>
+      <div id='blocker1'></div>
+      <div id='blocker1'></div>
 
 
-        
-       {/* <div id="heading" >
-              <input type="text"/>
-              <span>Heading</span>
-           </div>*/}
-        <div id='blocker1'></div>
-        <img className='imageUpload' src={uploadImageSrc || uploadImage} id='picture-pic' alt="Uploaded" />
-      <label className='uploadImageLabel' htmlFor="input-file">Upload Image</label>
-      <input id='input-file' type="file" accept='image/jpeg, image/png, image/jpg' onChange={handleFileChange} />
+      <div className="form-groupIngredient">
+        <label htmlFor="ingredientInput">Ingredients</label>
+        {[...Array(counter)].map((_, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              id={`ingredientInput${index}`}
+              name={`Ingredient ${index + 1}`}
+              value={ingredients[index]}
+              onChange={(e) => handleChange(index, e.target.value)}
+            />
+            <label htmlFor={`ingredientInput${index}`}>{index + 1}</label>
+          </div>
+        ))}
+      </div>
 
-   
-   <div id='blocker1'></div>
-   <div id='blocker1'></div>
-   <div id='blocker1'></div>
+      <button className='addOneMoreIngredient'  onClick={incrementCounter}>Add Ingredient</button>
+      <div id='blocker1'></div>
+      <div id='blocker1'></div>
 
-
-   <div className="form-groupIngredient">
-      <label htmlFor="ingredientInput">Ingredients</label>
-      {[...Array(counter)].map((_, index) => (
-        <div key={index}>
-          <input
-            type="text"
-            id={`ingredientInput${index}`}
-            name={`Ingredient ${index + 1}`}
-            value={ingredients[index]}
-            onChange={(e) => handleChange(index, e.target.value)}
-          />
-          <label htmlFor={`ingredientInput${index}`}>{index + 1}</label>
-        </div>
-      ))}
-    </div>
-
-    <button className='addOneMoreIngredient'  onClick={incrementCounter}>Add Ingredient</button>
-        <div id='blocker1'></div>
-        <div id='blocker1'></div>
-
-        <div className='discription'><textarea name="Description" placeholder='Description' onChange={(e) => { setdescription(e.target.value); }}></textarea>
+      <div className='discription'><textarea name="Description" placeholder='Description' onChange={(e) => { setdescription(e.target.value); }}></textarea>
         <span className='discriptionSpan'>Description</span>
-        </div>
+      </div>
 
-        
-        <button className='addOneMoreIngredient'  onClick={addRecipe}>Save Recipe</button>
+          
+      <button className='addOneMoreIngredient'  onClick={addRecipe}>Save Recipe</button>
        
       
     </div> 
-    </div>
-</div>
+  </div>
+</body>
+
 )};
