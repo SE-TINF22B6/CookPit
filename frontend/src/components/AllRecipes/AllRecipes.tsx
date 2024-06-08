4392778f1d6dee3e21c1db3103363f90524f2ff2
Simon <sihajek@aol.com>
2024-06-08 12:58:36 +0200

    removed fav button; added download button to all recipes; replaced dummy data with real data in every recipe; added pictures to data in frontend; added steps and ingredients to recipes;

--- frontend/src/components/AllRecipes/AllRecipes.tsx
+++ frontend/src/components/AllRecipes/AllRecipes.tsx
@@ -3,7 +3,7 @@ import "./AllRecipes.css";
 
 export default function AllRecipes({ allRecipes }: { allRecipes: any[] }) {
   const items = allRecipes.map((recipe) => {
-    const img = `data:image/jpeg;base64,${recipe.picture}`; 
+    const img = `data:image/jpeg;base64,${recipe.picture}`;
     const time = Number(recipe.cook_time) + Number(recipe.prep_time);
     return (
       <DisplayRecipe
@@ -12,6 +12,11 @@ export default function AllRecipes({ allRecipes }: { allRecipes: any[] }) {
         title={recipe.name}
         rating={recipe.rating}
         time={time}
+        description={recipe.description}
+        prepTime={recipe.prep_time}
+        cookTime={recipe.cook_time}
+        ingredients={recipe.ingredients}
+        steps={recipe.steps}
       />
     );
   });
