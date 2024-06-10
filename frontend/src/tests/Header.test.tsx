64f87e99a5c2a19e406dc5caa78db2d4c7583713
Simon <sihajek@aol.com>
2024-06-10 10:41:40 +0200

    test zeugs

--- frontend/src/tests/Header.test.tsx
+++ frontend/src/tests/Header.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Header from '../components/Header/Header'
+import React from "react";
+import { describe, expect, test } from "@jest/globals";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Header from "../components/Header/Header";
 
-test('renders the header of the website', () => {
-    render(<Header />);
-    const WebsiteName = screen.getByText("CookPit");
-    expect(WebsiteName).toBeInTheDocument();
-  });
+test("renders the header of the website", () => {
+  render(<Header />);
+  const WebsiteName = screen.getByText("CookPit");
+  expect(WebsiteName).toBeInTheDocument();
+});
